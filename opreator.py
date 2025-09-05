598da4264bfcf470b0aa1f5bafa06f24dc8c5d1d
Shubhabrata7384 <145095724+Shubhabrata7384@users.noreply.github.com>
2025-09-05 19:50:05 +0530

    Create opreator.py

---
+++ opreator.py
@@ -0,0 +1,14 @@
+#arithmetic operators
+a = 10
+b = 5
+print(a + b)  # Addition
+#assignment operators
+c=a + b
+print(c)  # Output of addition
+#comparison operators
+d= a > b
+print(d)  # Output of comparison
+
+#logical operators
+e = True or False
+print(e)  # Output of logical operation
