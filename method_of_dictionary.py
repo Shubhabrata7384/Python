ef7e2c9f0a3fc4cdd6f0283b69d1511234062962
Shubhabrata7384 <145095724+Shubhabrata7384@users.noreply.github.com>
2025-09-19 23:51:08 +0530

    Create method_of_dictionary.py

---
+++ method_of_dictionary.py
@@ -0,0 +1,12 @@
+marks={
+    "Shubhabrata":99,
+    "Siddharth":95,
+    "Siddhant":92,
+    "Shivam":88
+}
+print(marks,type(marks))
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+print(marks.update({"Shubhabrata":100 }))  # Corrected key to string
+print(marks)
