5d1ffa5599acebb8ebd72a2c88ec9e5ba3a6613d
Shubhabrata7384 <145095724+Shubhabrata7384@users.noreply.github.com>
2025-09-21 23:31:43 +0530

    Create problem1E.py

---
+++ problem1E.py
@@ -0,0 +1,9 @@
+#wap to translate words from Hindi to English
+words={
+    "namaste": "hello",
+    "shukriya": "thank you",
+    "alvida": "goodbye",
+    "suno": "listen",
+}
+word=input("Enter a word you want to translate: ")
+print(words[word])
