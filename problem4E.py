f09042433275fb5f60789b8fdfb8de1eb36a80c5
Shubhabrata7384 <145095724+Shubhabrata7384@users.noreply.github.com>
2025-09-23 14:41:05 +0530

    Create problem4E.py

---
+++ problem4E.py
@@ -0,0 +1,15 @@
+#wap to create a dictionary with names and languages
+d={}
+name=input("Enter your name: ")
+language=input("Enter your language: ")
+d.update({name: language})
+name=input("Enter your name: ")
+language=input("Enter your language: ")
+d.update({name: language})
+name=input("Enter your name: ")
+language=input("Enter your language: ")
+d.update({name: language})
+name=input("Enter your name: ")
+language=input("Enter your language: ")
+d.update({name: language})
+print(d)
