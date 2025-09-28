f91eacc5ffac3ca2dc0b8cfcf5cd045c2a79b455
Shubhabrata7384 <145095724+Shubhabrata7384@users.noreply.github.com>
2025-09-28 23:41:06 +0530

    Create if_elif_else_ladder.py

---
+++ if_elif_else_ladder.py
@@ -0,0 +1,14 @@
+a=int(input("Enter your age:"))
+
+# if elif else ladder statement
+if(a>=18):
+    print("You are eligible to vote.")
+    print("You can vote in the upcoming elections.")
+elif(a<0):
+    print("You are enter invaild age.")
+
+else:
+    print("You are not eligible to vote.")
+
+print("Thank you for checking your eligibility.")
+print("Have a great day!")
