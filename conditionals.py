d52f285d25b5d7b03d43eeeb2a3456831f84ae17
Shubhabrata7384 <145095724+Shubhabrata7384@users.noreply.github.com>
2025-09-28 23:41:48 +0530

    Create conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+a=int(input("Enter your age:"))
+
+#if else statement
+if(a>=18):
+    print("You are eligible to vote.")
+    print("You can vote in the upcoming elections.")
+
+else:
+    print("You are not eligible to vote.")
+
+print("Thank you for checking your eligibility.")
+print("Have a great day!")
