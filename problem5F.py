5205626217bff7d23b724b3ff421fb55b340252e
Shubhabrata7384 <145095724+Shubhabrata7384@users.noreply.github.com>
2025-10-30 10:58:48 +0530

    Create problem5F.py

---
+++ problem5F.py
@@ -0,0 +1,14 @@
+marks=input("Enter your Marks:")
+
+if(100=>marks and 90<=marks):
+    print("Your Grade is:Ex")
+elif(90>=marks and 80<=marks):
+    print("Your Grade is:A+")
+elif(80>=marks and 70<=marks):
+    print("Your Grade is:A")
+elif(70>=marks and 60<=marks):
+    print("Your Grade is:B+")
+elif(60>=marks and 50<=marks):
+    print("Your Grade is:B")
+elif(50>marks):
+    print("Your Grade is:Fail")    
