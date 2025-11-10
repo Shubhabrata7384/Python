c018ddf09d9ae6a142a7051749fb53e7e611244d
Shubhabrata7384 <145095724+Shubhabrata7384@users.noreply.github.com>
2025-11-11 00:14:43 +0530

    Create function_intro.py

---
+++ function_intro.py
@@ -0,0 +1,19 @@
+
+#define a funtion
+def avg():
+    a=int(input("Enter first number: "))
+    b=int(input("Enter second number: "))
+    c=int(input("Enter third number: "))
+    average=(a+b+c)/3
+    print("The average of three numbers is:",average)
+
+
+
+#call the function
+avg()
+print("thanks for using the avg function")
+avg()
+print("thanks for using the avg function")
+avg()
+print("thanks for using the avg function")
+ 
